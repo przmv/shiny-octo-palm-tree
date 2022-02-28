dad75ef460bb2a662793e311ca55b11cdd9eb793
Petr Razumov <p@rzmv.cc>
2022-02-28 18:16:22 +0300

    Adding the actual test

--- regexp/regexp_test.go
+++ regexp/regexp_test.go
@@ -2,8 +2,36 @@ package regexp_test
 
 import (
 	"testing"
+
+	"githib.com/przmv/shiny-octo-palm-tree/regexp"
 )
 
+const (
+	validString   = `23-ab-48-caba-56-haha`
+	invalidString = `aaa-bbb-ccc-ddd-eee-fff`
+)
+
+var testCases = []struct {
+	String  string
+	IsValid bool
+}{
+	{
+		String:  validString,
+		IsValid: true,
+	},
+	{
+		String:  invalidString,
+		IsValid: false,
+	},
+}
+
 func TestTestValidity(t *testing.T) {
-	t.Skip()
+	for _, tc := range testCases {
+		s := tc.String
+		expected := tc.IsValid
+		got := regexp.TestValidity(s)
+		if expected != got {
+			t.Errorf("%q: expected %v, got %v", s, expected, got)
+		}
+	}
 }
