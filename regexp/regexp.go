c220c725bf9c0e5e5da81e804beb72e4a45e23b9
Petr Razumov <p@rzmv.cc>
2022-03-01 16:33:51 +0300

    Adding test for StoryStats

--- regexp/regexp.go
+++ regexp/regexp.go
@@ -53,3 +53,11 @@ func WholeStrory(s string) (string, error) {
 	a := words.FindAllString(s, -1)
 	return strings.Join(a, " "), nil
 }
+
+// StoryStats returns the shortest word, the longest word, the average word
+// length, the list (or empty list) of all words from the story that have the
+// length the same as the average length rounded up and down.
+func StoryStats(s string) (shortest, longest string, avg float64, asAvg []string, err error) {
+	// TODO
+	return
+}
--- regexp/regexp_test.go
+++ regexp/regexp_test.go
@@ -1,6 +1,7 @@
 package regexp_test
 
 import (
+	"reflect"
 	"testing"
 
 	"githib.com/przmv/shiny-octo-palm-tree/regexp"
@@ -96,6 +97,50 @@ func TestWholeStory(t *testing.T) {
 		}
 		if expectedErr != err {
 			t.Errorf("%q: expected error %v, got %v", s, expectedErr, err)
+		}
+	}
+}
+
+var storyStatsTestCases = []struct {
+	String   string
+	Error    error
+	Shortest string
+	Longest  string
+	Avg      float64
+	AsAvg    []string
+}{
+	{
+		String:   validString,
+		Error:    nil,
+		Shortest: "ab",
+		Longest:  "haha",
+		Avg:      3.3333333333333335,
+		AsAvg:    []string{"caba", "haha"},
+	},
+	{
+		String: invalidString,
+		Error:  regexp.InvalidStringError,
+	},
+}
+
+func TestStoryStats(t *testing.T) {
+	for _, tc := range storyStatsTestCases {
+		s := tc.String
+		shortest, longest, avg, asAvg, err := regexp.StoryStats(s)
+		if shortest != tc.Shortest {
+			t.Errorf("%q: expected %q, got %q", s, tc.Shortest, shortest)
+		}
+		if longest != tc.Longest {
+			t.Errorf("%q: expected %q, got %q", s, tc.Longest, longest)
+		}
+		if avg != tc.Avg {
+			t.Errorf("%q: expected %v, got %v", s, tc.Avg, avg)
+		}
+		if !reflect.DeepEqual(asAvg, tc.AsAvg) {
+			t.Errorf("%q: expected %v, got %v", s, tc.AsAvg, asAvg)
+		}
+		if err != tc.Error {
+			t.Errorf("%q: expected error %v, got %v", s, tc.Error, err)
 		}
 	}
 }
