8b04e23b91e047ecf2abb03d7d38ca7bc9803f27
Petr Razumov <p@rzmv.cc>
2022-03-03 16:12:07 +0300

    Adding the actual AverageNumber implementation

--- strings/strings.go
+++ strings/strings.go
@@ -1,8 +1,11 @@
 package strings
 
 import (
+	"strconv"
 	"strings"
 	"unicode"
+
+	"githib.com/przmv/shiny-octo-palm-tree/errors"
 )
 
 // TestValidity checks if a string complies with the format.
@@ -31,6 +34,21 @@ func TestValidity(s string) bool {
 
 // AverageNumber returns an average number from all the numbers from the string s.
 func AverageNumber(s string) (float64, error) {
-	// TODO
-	return 0, nil
+	if !TestValidity(s) {
+		return 0, errors.InvalidStringError
+	}
+	a := strings.Split(s, "-")
+	sum, n := 0, 0
+	for i, s := range a {
+		if i%2 == 0 {
+			i, err := strconv.Atoi(s)
+			if err != nil {
+				return 0, err
+			}
+			sum += i
+			n++
+		}
+	}
+	avg := (float64(sum)) / (float64(n))
+	return avg, nil
 }
