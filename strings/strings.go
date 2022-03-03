f67359b9f78fadf2041f495ff9d8e5ca9e10075e
Petr Razumov <p@rzmv.cc>
2022-03-03 15:36:38 +0300

    Adding the actual TestValidity implementation

--- strings/strings.go
+++ strings/strings.go
@@ -1,7 +1,30 @@
 package strings
 
+import (
+	"strings"
+	"unicode"
+)
+
 // TestValidity checks if a string complies with the format.
 func TestValidity(s string) bool {
-	// TODO
-	return false
+	a := strings.Split(s, "-")
+	if len(a) < 2 {
+		return false
+	}
+	for i, s := range a {
+		if i%2 == 0 {
+			for _, r := range s {
+				if !unicode.IsNumber(r) {
+					return false
+				}
+			}
+		} else {
+			for _, r := range s {
+				if !unicode.Is(unicode.Latin, r) {
+					return false
+				}
+			}
+		}
+	}
+	return true
 }
